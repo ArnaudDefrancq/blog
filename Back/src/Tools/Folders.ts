e85a62c87e8c93451a5ff8c05169327a4bb5738f
unknown <arnaud.defrancq@laposte.net>
2025-03-04 11:57:27 +0100

    [+] Tools : Ajout de la class Folders pour gérer les fichiers

---
+++ Back/src/Tools/Folders.ts
@@ -0,0 +1,58 @@
+import path from "path";
+import * as fs from "fs/promises"
+
+export class Folders {
+    private static async folderExists(folderPath: string): Promise<boolean> {
+        try {
+            await fs.access(folderPath);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    public static async createFolder(id: string) {
+        try {
+            const postFolder = path.join(__dirname, '../Images/imgPost', id);
+            if (!(await Folders.folderExists(postFolder))) {
+                await fs.mkdir(postFolder, { recursive: true });
+                return true;
+            }
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    }
+
+    public static async deleteAllFolders(id: string): Promise<boolean> {
+        try {
+            const postFolder = path.join(__dirname, '../Images/imgPost', id);
+
+            if (!(await Folders.folderExists(postFolder))) {
+                return false;
+            }
+            
+            await fs.rm(postFolder, { recursive: true })
+            return true;
+        } catch (error) {
+            console.error('Erreur lors de la suppression des dossiers | ' + error);
+            return false;
+        }
+    }
+
+    public static async deleteFolder(id: string, nameFile: string): Promise<boolean> {
+        try {
+            const postFolder = path.join(__dirname, '../Images/imgPost', id, nameFile);
+
+            if (!(await Folders.folderExists(postFolder))) {
+                console.error('Le fichier existe pas');
+                return false
+            }
+            await fs.rm(postFolder, { recursive: true });
+            return true;
+        } catch (error) {
+            console.error('Erreur lors de la suppression des dossiers | ' + error);
+            return false;
+        }
+    }
+}
