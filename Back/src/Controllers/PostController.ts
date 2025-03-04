06456f0e834842ff9278908c386b7fcbc2261551
unknown <arnaud.defrancq@laposte.net>
2025-03-04 11:58:59 +0100

    [+] PostController : CreatePost (Pas OK)

--- Back/src/Controllers/PostController.ts
+++ Back/src/Controllers/PostController.ts
@@ -4,7 +4,7 @@ import { Post } from "../Types/Post";
 export class PostController {
     public static async createPost(req: Request, res: Response, next: NextFunction): Promise<Post | any> {
         try {
-
+            
         } catch (error) {
             return res.status(500).json({error});
         }
