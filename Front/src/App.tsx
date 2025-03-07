0ab419bec9d88162b282adc7f7776ceedf9a1f2d
unknown <arnaud.defrancq@laposte.net>
2025-03-07 11:35:10 +0100

    [~] App : Ajout des routes OK

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,8 +1,23 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import ConnectionPage from "./Pages/ConnectionPages"
+import HomePage from "./Pages/HomePage"
+import ProfilPage from "./Pages/ProfilPage"
+import ParametersPage from "./Pages/ParametersPage"
+import ErrorPage from "./Pages/ErrorPage"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-9xl">Tailwinds</h1>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ConnectionPage />} />
+            <Route path="/feeds" element={<HomePage />} />
+            <Route path="/profil/:id" element={<ProfilPage />} />
+            <Route path="/profil/parameters/:id" element={<ParametersPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
