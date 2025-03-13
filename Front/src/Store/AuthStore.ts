1d20a6d8bb9d8894a6ac2e5daef65480aed0d74c
unknown <arnaud.defrancq@laposte.net>
2025-03-13 08:53:38 +0100

    [~] UserStore : ajout du token dans le store

--- Front/src/Store/AuthStore.ts
+++ Front/src/Store/AuthStore.ts
@@ -2,14 +2,16 @@ import { create } from "zustand";
 
 type AuthStoreType = {
     user_id: number | null,
-    role: number | null,
-    setUser: (user_id: number, role: number) => void,
+    role_id: number | null,
+    token: string | null, 
+    setUser: (user_id: number, role_id: number, token: string) => void,
     logOut: () => void
 }
 
 export const useAuthStore = create<AuthStoreType>((set) => ({
     user_id: null,
-    role: null,
-    setUser: (user_id, role) => set({user_id, role}),
-    logOut: () => set({user_id: null, role: null})
+    role_id: null,
+    token: null,
+    setUser: (user_id, role_id, token) => set({user_id, role_id, token}),
+    logOut: () => set({user_id: null, role_id: null, token: null})
 }))
