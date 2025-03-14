0bd29d4c4c7e1029a977f217a75da7eef52b3016
unknown <arnaud.defrancq@laposte.net>
2025-03-14 17:58:25 +0100

    [+] Store : commentStore + likePostStore

---
+++ Front/src/Store/CommentStore.ts
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+import { Comment, CommentWithUser } from "../Types/Comment";
+import { CommentController } from "../Controllers/CommentController";
+
+type CommentStoreType = {
+    comments: Array<CommentWithUser>,
+    fetchComment: (token: string) => Promise<Array<CommentWithUser>>,
+    fetchOneComment: (id: number, token: string) => Promise<CommentWithUser | null>,
+    createComment: (comment: Comment, token: string) => Promise<void>,
+    deleteComment: (id: number, token: string) => Promise<void>
+}
+
+
+export const usePostStore = create<CommentStoreType>((set) => ({
+    comments: [],
+    fetchComment: async (token: string): Promise<Array<CommentWithUser>> => {
+        try {
+            const res = await CommentController.getAllComment(token);
+            set({ comments: res });
+            return res;
+        } catch (error) {
+            console.error(error);
+            set({ comments: [] });
+            return [];
+        }
+    },
+
+    fetchOneComment: async (id: number, token: string): Promise<CommentWithUser | null> => {
+        try {
+            const res = await CommentController.getOneComment(id, token);
+            if (res) {
+                return res;
+            }
+            return null;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+
+    createComment: async (comment: Comment, token: string): Promise<void> => {
+        try {
+            const res = await CommentController.createComment(comment, token); 
+            if (res) {
+                const newComment = (await CommentController.getAllComment(token))[0];
+                set((state) => ({ comments: [...state.comments, newComment] }));
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    deleteComment: async (id: number, token: string): Promise<void> => {
+        try {
+            const res = await CommentController.deleteComment(id, token); 
+            if (res) {
+                set((state) => ({ comments: state.comments.filter((comment) => comment.id_comment !== id) }));
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}));
---
+++ Front/src/Store/LikePostStore.ts
@@ -0,0 +1,62 @@
+import { create } from "zustand";
+import { Like_post } from "../Types/Like_post";
+import { LikePostController } from "../Controllers/LikePostController";
+
+type LikePostStoreType = {
+    likePosts: Array<Like_post>,
+    fetchLikePost: (token: string) => Promise<Array<Like_post>>,
+    fetchOneLikePost: (id: number, token: string) => Promise<Like_post | null>,
+    createLikePost: (likePost: Like_post, token: string) => Promise<void>,
+    deleteLikePost: (id: number, token: string) => Promise<void>
+}
+
+export const usePostStore = create<LikePostStoreType>((set) => ({
+    likePosts: [],
+    fetchLikePost: async (token: string): Promise<Array<Like_post>> => {
+        try {
+            const res = await LikePostController.getAllLikePost(token);
+            set({ likePosts: res });
+            return res;
+        } catch (error) {
+            console.error(error);
+            set({ likePosts: [] });
+            return [];
+        }
+    },
+
+    fetchOneLikePost: async (id: number, token: string): Promise<Like_post | null> => {
+        try {
+            const res = await LikePostController.getOneLikePost(id, token);
+            if (res) {
+                return res;
+            }
+            return null;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+
+    createLikePost: async (likePost: Like_post, token: string): Promise<void> => {
+        try {
+            const res = await LikePostController.createLikePost(likePost, token); 
+            if (res) {
+                const newLikePost = (await LikePostController.getAllLikePost(token))[0];
+                set((state) => ({ likePosts: [...state.likePosts, newLikePost] }));
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    deleteLikePost: async (id: number, token: string): Promise<void> => {
+        try {
+            const res = await LikePostController.deleteLikePost(id, token); 
+            if (res) {
+                set((state) => ({ likePosts: state.likePosts.filter((likePost) => likePost.id_like_post !== id) }));
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}));
