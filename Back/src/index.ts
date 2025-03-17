ffefae2f803c4d91284fe0a835b94bbc98151068
unknown <arnaud.defrancq@laposte.net>
2025-03-17 13:05:32 +0100

    [~] Cors : Modification dans la gestion des cors

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -17,10 +17,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(cors({
-//   origin: 'http://localhost:5173/',
-//   credentials: true
-// }))
 const corsOptions ={
   origin:'http://localhost:5173', 
   credentials:true,            //access-control-allow-credentials:true
@@ -28,18 +24,18 @@ const corsOptions ={
 
 app.use(cors(corsOptions))
 // app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//     );
-//     next();
-// });
+//   res.setHeader("Access-Control-Allow-Origin", 'http://localhost:5173');
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });;
 
 app.get("/", (req, res) => {
   res.status(200).json({
