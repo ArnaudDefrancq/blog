20a1273d47705f36a9664c3ce32cc30517d82e2c
unknown <arnaud.defrancq@laposte.net>
2025-03-18 13:40:21 +0100

    [+] Feeds : Recherche des Posts disponible au chargement de la page OK

--- Front/src/Components/homePage/feeds/Feeds.tsx
+++ Front/src/Components/homePage/feeds/Feeds.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import { usePostStore } from '../../../Store/PostStore';
+import Card from './card/Card';
 
 const Feeds: React.FunctionComponent = () => {
+
+  const { posts, fetchPost } = usePostStore();
+
+  React.useEffect(() => {
+      fetchPost()
+  }, [fetchPost])
+
   return (
     <>
-        
+      <section>
+        {
+          posts?.map((post) => {
+            return <Card posts={post} key={post.id_post}/>
+          })
+        }  
+      </section>        
     </>
   );
 };
