aeb34183ac048c285d663f0aa466c832b7c616f4
unknown <arnaud.defrancq@laposte.net>
2025-03-18 14:16:07 +0100

    [~] HomePage : Ajout des balises header / main / footer

--- Front/src/Pages/HomePage.tsx
+++ Front/src/Pages/HomePage.tsx
@@ -6,9 +6,17 @@ import Footer from '../Components/footer/Footer';
 const HomePage: React.FunctionComponent = () => {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer/>
+    <div className='flex flex-col min-h-screen'>
+      <header className='flex justify-end bg-stone-50'>
+        <Header />
+      </header>
+      <main className='flex-grow bg-colorTwo'>
+        <Home />
+      </main>
+      <footer className='bg-colorFive h-25 flex justify-center items-center'>
+        <Footer/>
+      </footer>
+    </div>
     </>
   );
 };
