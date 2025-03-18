7120d68c4ca1e6aca1aee87f3e15056e9cba1e02
unknown <arnaud.defrancq@laposte.net>
2025-03-18 14:25:12 +0100

    [+] Home : Ajout du btn pour ajouter un post (Style ok)

--- Front/src/Components/homePage/Home.tsx
+++ Front/src/Components/homePage/Home.tsx
@@ -5,6 +5,9 @@ const Home: React.FunctionComponent = () => {
   return (
     <>
       <Feeds />
+      <button className="fixed bottom-40 right-20 bg-colorFive text-white px-8 py-2 rounded-full shadow-lg hover:bg-colorFour hover:text-black cursor-pointer text-xl">
+        Publier
+      </button>
     </>
   );
 };
