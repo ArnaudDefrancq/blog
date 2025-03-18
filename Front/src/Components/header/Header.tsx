3df681ce59ba37f38e5ccf0b794099ca464cccc8
unknown <arnaud.defrancq@laposte.net>
2025-03-18 14:15:04 +0100

    [~] Header : Suppr de la balise header dans ce components

--- Front/src/Components/header/Header.tsx
+++ Front/src/Components/header/Header.tsx
@@ -5,9 +5,7 @@ import NavBar from './navBar/NavBar';
 const Header: React.FunctionComponent = () => {
   return (
     <>
-        <header className='flex justify-end bg-stone-50'>
-          <NavBar />
-        </header>
+      <NavBar />
     </>
   ) ;
 };
