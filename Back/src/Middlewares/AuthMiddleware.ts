21096a3b34a8856832c6e48dcf49813d2cb763cb
unknown <arnaud.defrancq@laposte.net>
2025-03-18 10:14:01 +0100

    [~] Middleware : Suppr des console.log

--- Back/src/Middlewares/AuthMiddleware.ts
+++ Back/src/Middlewares/AuthMiddleware.ts
@@ -5,7 +5,6 @@ export class AuthMiddleware {
     
     public static async authenticateJWT(req: Request, res: Response, next: NextFunction): Promise<void | any> {
         const token = req.cookies.token
-        console.log(token)
         if (!token) {
           res.status(401).json({ message: "Accès non autorisé" });
           return;
