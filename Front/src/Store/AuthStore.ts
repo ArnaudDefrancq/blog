87300f22390d3ae14dc831fb8726ca538918fe2e
unknown <arnaud.defrancq@laposte.net>
2025-03-18 10:16:56 +0100

    [+] Store : Ajout de la route pour obtenir les info de connexion

--- Front/src/Store/AuthStore.ts
+++ Front/src/Store/AuthStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
+import { UserController } from "../Controllers/UserController";
 
 type AuthStoreType = {
     user_id: number | null,
     role_id: number | null,
     setUser: (user_id: number, role_id: number) => void,
+    fetchUser: () => void,
     logOut: () => void
 }
 
@@ -11,5 +13,15 @@ export const useAuthStore = create<AuthStoreType>((set) => ({
     user_id: null,
     role_id: null,
     setUser: (user_id, role_id) => set({user_id, role_id}),
+    fetchUser: async () => {
+        try {
+            const res = await UserController.getInfoconnection();
+            console.log(res)
+            set({user_id: null, role_id: null})
+        } catch (error) {
+            console.log(error);
+            set({user_id: null, role_id: null})
+        }
+    },
     logOut: () => set({user_id: null, role_id: null})
 }))
