2c740f531d4b2742e474f775f7fdb24dbede8d70
unknown <arnaud.defrancq@laposte.net>
2025-03-18 13:28:59 +0100

    [~] Controller : Modif du retour de la method getInfo

--- Front/src/Controllers/UserController.ts
+++ Front/src/Controllers/UserController.ts
@@ -5,11 +5,11 @@ export class UserController {
     public static async signUp(auth: Auth): Promise<number | { code: number } | undefined> {
         return await UserModel.signUp(auth);
     }
-    public static async signIn(auth: Auth): Promise<void> {
+    public static async signIn(auth: Auth): Promise<boolean | void> {
         return await UserModel.signIn(auth);
     }
 
-    public static async getInfoconnection(): Promise<void> {
+    public static async getInfoconnection(): Promise<{user_id: number, role: number} | void> {
         return await UserModel.getInfoConnection();
     }
 }
