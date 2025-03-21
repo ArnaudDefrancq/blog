2eb80e5526a466c7a7c8d46a957b024c991e4d5a
unknown <arnaud.defrancq@laposte.net>
2025-03-21 13:43:05 +0100

    [~] HomePage : redirection vers le post au clique du btn

--- Front/src/Components/homePage/feeds/card/Card.tsx
+++ Front/src/Components/homePage/feeds/card/Card.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { PostWithUser } from '../../../../Types/Post';
 import Author from './author/Author';
+import { useNavigate } from 'react-router-dom';
 
 interface ICardProps {
   posts : PostWithUser
 }
 
 const Card: React.FunctionComponent<ICardProps> = ({posts}) => {
+  const navigate = useNavigate()
+
+  const handleClick = (): void => {
+    navigate(`/post/${posts.id_post}`)
+  }
+
   return (
     <>
       <div className="bg-white shadow-md rounded-lg p-4 my-4 mx-auto w-300">
@@ -20,7 +27,7 @@ const Card: React.FunctionComponent<ICardProps> = ({posts}) => {
           <p>{posts.content.length > 100 ? posts.content.substring(0, 100) + '...' : posts.content}</p>
         </div>
         <div className="mt-4 flex justify-end">
-            <button className="bg-colorTwo text-white px-4 py-2 rounded-lg hover:bg-colorThree  hover:text-black transition delay-0.3 cursor-pointer">
+            <button className="bg-colorTwo text-white px-4 py-2 rounded-lg hover:bg-colorThree  hover:text-black transition delay-0.3 cursor-pointer" onClick={handleClick}>
               Plus de détails
             </button>
         </div>
