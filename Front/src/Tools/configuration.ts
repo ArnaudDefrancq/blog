859ee753c64f4f8eda6cbf6047dcc38a8e5caf8e
unknown <arnaud.defrancq@laposte.net>
2025-03-24 16:57:35 +0100

    [~] Config : Modif regex title

--- Front/src/Tools/configuration.ts
+++ Front/src/Tools/configuration.ts
@@ -1,5 +1,5 @@
 export const REGEX_MAIL: RegExp = /^(?!\.)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$/;
 export const REGEX_PSEUDO: RegExp = /^[a-zA-Z\s].{3,20}$/; 
-export const REGEX_TITLE: RegExp = /^[a-zA-Z\s].{3,50}$/; 
+export const REGEX_TITLE: RegExp = /^[a-zA-Z\s].{3,49}$/; 
 export const REGEX_TEXTE: RegExp = /^[a-zA-Z\s].{3,200}$/;
 export const REGEX_PASSWORD: RegExp =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,}$/;
