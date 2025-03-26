7f0de6ccd76d67a4218aaea41df4eeaf4f3a2a97
unknown <arnaud.defrancq@laposte.net>
2025-03-26 10:15:51 +0100

    [+] ProfilPage : Mise en place de la page profil

--- Front/src/Components/profilPage/Profil.tsx
+++ Front/src/Components/profilPage/Profil.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Profil: React.FunctionComponent = () => {
+
+  const { id } = useParams();
+
   return (
     <>
-        <h1>Profil</h1>
+      <h1>Profil</h1>
     </>
   );
 };
--- Front/src/Pages/ProfilPage.tsx
+++ Front/src/Pages/ProfilPage.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
+import Header from '../Components/header/Header';
+import Footer from '../Components/footer/Footer';
+import Profil from '../Components/profilPage/Profil';
 
 const ProfilPage: React.FunctionComponent = () => {
   return (
-   <>
-   
+   <>        
+    <div className='flex flex-col min-h-screen'>
+      <header className='flex justify-end bg-stone-50'>
+        <Header />
+      </header>
+      <main className='flex-grow bg-colorTwo'>
+        <Profil />
+      </main>
+      <footer className='bg-colorFive h-25 flex justify-center items-center'>
+        <Footer/>
+      </footer>
+    </div>
    </> 
   );
 };
